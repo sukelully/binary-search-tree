a5960da482ed6bff383e693983c2cf1fae6da579
Luke Sullivan <luke@sukelully.dev>
2025-06-14 15:06:39 +0100

    add driver logic, complete project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
 import Tree from "./Tree";
 
-const array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const tree = new Tree(array);
+const arr: number[] = [];
+for (let i = 0; i < 100; i++) {
+    const randInt: number = Math.floor(Math.random() * (32 - 1)) + 1;
+    arr.push(randInt);
+}
 
-tree.deleteItem(1);
+const tree = new Tree(arr);
 tree.prettyPrint(tree.root);
-console.log(tree.isBalanced());
+console.log(`tree is balanced: ${tree.isBalanced()}`);
+
+const levelOrderArr: number[] = [];
+tree.levelOrder((node) => levelOrderArr.push(node.val));
+console.log(`Level Order: ${levelOrderArr.join(" ")}`);
+
+const preOrderArr: number[] = [];
+tree.preOrder((node) => preOrderArr.push(node.val));
+console.log(`Pre Order: ${preOrderArr.join(" ")}`);
+
+const postOrderArr: number[] = [];
+tree.postOrder((node) => postOrderArr.push(node.val));
+console.log(`Post Order: ${postOrderArr.join(" ")}`);
+
+const inOrderArr: number[] = [];
+tree.inOrder((node) => inOrderArr.push(node.val));
+console.log(`Pre Order: ${inOrderArr.join(" ")}`);
+
+// Unbalance tree
+tree.insert(42);
+tree.insert(69);
+tree.insert(52);
+
+tree.prettyPrint(tree.root);
+console.log(`Tree is balanced: ${tree.isBalanced()}`);
+
+// Rebalance tree
 tree.rebalance();
 tree.prettyPrint(tree.root);
-console.log(tree.isBalanced());
+console.log(`Tree is balanced: ${tree.isBalanced()}`);
